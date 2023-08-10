055bc25f64f0847aab2aa3f0492ff2f92cc226d9
Aitor Martin <aitor.martin@bosonit.com>
2023-08-10 08:58:06 +0200

    Cambio clase Block13TestingApplicationTest Linea 15

--- Bloque 13/Block13Testing/src/test/java/com/example/Block13Testing/Block13TestingApplicationTests.java
+++ Bloque 13/Block13Testing/src/test/java/com/example/Block13Testing/Block13TestingApplicationTests.java
@@ -1,13 +1,22 @@
 package com.example.Block13Testing;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Block13TestingApplicationTests {
 
+
+	@Autowired
+	private Block13TestingApplication application;
+
 	@Test
 	void contextLoads() {
+
+		assert(application != null);
+
 	}
+
 
 }
