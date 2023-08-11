df0d250b9bfee7f6923bfc5ab2d4c104ea5abe20
Aitor Martin <aitor.martin@bosonit.com>
2023-08-11 11:12:00 +0200

    Subo el proyecto SpringSecurity

---
+++ Bloque 14/block14SpringSecurity/src/main/java/com/formacion/block14SpringSecurity/Block14SpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.formacion.block14SpringSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Block14SpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Block14SpringSecurityApplication.class, args);
+	}
+
+}
