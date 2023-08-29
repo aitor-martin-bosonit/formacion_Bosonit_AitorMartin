512ee9193f5f82c7b2aee2592b05439f9e13af21
Aitor Martin <aitor.martin@bosonit.com>
2023-08-29 12:33:34 +0200

    "correccion 4 Error RestTemplate crear clase RestTemplateConfig"

---
+++ bloque16.springCloud/trip-frontend/src/main/java/com/formacion/tripfrontend/application/RestTemplateConfig.java
@@ -0,0 +1,13 @@
+package com.formacion.tripfrontend.application;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+public class RestTemplateConfig {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+}
