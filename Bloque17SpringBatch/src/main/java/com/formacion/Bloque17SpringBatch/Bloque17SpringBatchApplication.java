15d70c95ef65acfcd924205477f9bdecf7ec80e9
Aitor Martin <aitor.martin@bosonit.com>
2023-08-30 08:40:40 +0200

    Bloque 17 Spring Batch

---
+++ Bloque17SpringBatch/src/main/java/com/formacion/Bloque17SpringBatch/Bloque17SpringBatchApplication.java
@@ -0,0 +1,13 @@
+package com.formacion.Bloque17SpringBatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bloque17SpringBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bloque17SpringBatchApplication.class, args);
+	}
+
+}
---
+++ Bloque17SpringBatch/src/test/java/com/formacion/Bloque17SpringBatch/Bloque17SpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formacion.Bloque17SpringBatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bloque17SpringBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
